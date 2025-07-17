fb3c9cf60b8b3705f0f4eae883953465ba9517d2
itc-s23014 <s23014@std.it-college.ac.jp>
2025-07-17 13:59:40 +0900

    コンフリクト直し

--- websns-app/flaskapp.session.sql
+++ websns-app/flaskapp.session.sql
@@ -1,12 +1,10 @@
 SELECT * FROM messages
 SELECT * FROM users
-<<<<<<< HEAD
+
 SELECT * FROM post
 RENAME TABLE messages TO Posts
 SELECT * FROM Posts
 ALTER TABLE Posts ADD COLUMN username VARCHAR(50);
 SHOW TABLES
-=======
+
 SELECT DATABASE();
->>>>>>> 1e5aa7d (push)
-SELECT * FROM Posts
