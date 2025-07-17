d3a78087d933a5dc9efe687622f4e4278b9cfc94
itc-s23014 <s23014@std.it-college.ac.jp>
2025-07-17 13:58:14 +0900

    __pycache消して、.gitignoreに書いた

--- websns-app/flaskapp.session.sql
+++ websns-app/flaskapp.session.sql
@@ -1,8 +1,12 @@
 SELECT * FROM messages
 SELECT * FROM users
+<<<<<<< HEAD
 SELECT * FROM post
 RENAME TABLE messages TO Posts
 SELECT * FROM Posts
 ALTER TABLE Posts ADD COLUMN username VARCHAR(50);
 SHOW TABLES
+=======
+SELECT DATABASE();
+>>>>>>> 1e5aa7d (push)
 SELECT * FROM Posts
